03146514e5554f5084fcedc90c4544157ffe0e5f
Evgeniy6232 <148517308+Evgeniy6232@users.noreply.github.com>
2026-02-21 18:10:15 +0700

    Partial implementation UserManager

---
+++ src/main/java/UserManager.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class UserManager implements Repository<User> {
+
+    private Map<String, User> users = new HashMap<>();
+
+    @Override
+    public void add(User user) {
+
+        if (user == null) {
+            throw new IllegalArgumentException("User cannot by null");
+        }
+
+
+        String username = user.username();
+        if (users.containsKey(username)) {
+            throw new IllegalArgumentException("User with username <" + username + "> already exists");
+        }
+
+        users.put(username, user);
+    }
+
+    @Override
+    public boolean remove(User user) {
+        if (user == null) {
+            return false;
+        }
+
+        User removed = users.remove(user.username());
+        return removed != null;
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        if (id == null || id.isBlank()) {
+            return Optional.empty();
+        }
+
+        User user = users.get(id);
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public int count() {
+        return users.size();
+    }
+
+    @Override
+    public void clear() {
+        users.clear();
+    }
+
+    public Optional<User> findByUsername(String username) {
+        if (username == null || username.isBlank()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(users.get(username));
+    }
+
+    public Optional<User> findByEmail(String email) {
+        if (email == null || email.isBlank()) {
+            return Optional.empty();
+        }
+
+        for(User user : users.values()) {
+            if (user.email().equals(email)){
+                return Optional.of(user);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+
+}
